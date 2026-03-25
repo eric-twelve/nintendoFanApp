b681f54a39c0e9bb28689b72441093481fdaebd9
EricAlvarezLinares <ericlinares2012@gmail.com>
2026-03-25 22:36:06 +0100

    EAC2 Exercici 4

--- src/app/components/barra-cerca/barra-cerca.component.ts
+++ src/app/components/barra-cerca/barra-cerca.component.ts
@@ -1,17 +1,23 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-barra-cerca',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './barra-cerca.component.html',
   styleUrls: ['./barra-cerca.component.scss']
 })
 export class BarraCercaComponent {
+
+  buscarText: string = '';
+
   @Output() buscar = new EventEmitter<string>();
 
-  onInput(event: any) {
-    this.buscar.emit(event.target.value);
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      this.buscar.emit(this.buscarText);
+    }
   }
 }
